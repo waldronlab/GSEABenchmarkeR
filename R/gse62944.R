1e9f6dd8ea74a5d09310bef2f59c9130ded93728
lgeistlinger <ludwig.geistlinger@bio.ifi.lmu.de>
2020-02-29 21:48:22 -0500

    loadEData: mode = cTD to obtain TCGA RNA-seq datasets from curatedTCGAData

--- R/gse62944.R
+++ R/gse62944.R
@@ -11,26 +11,38 @@
 #
 # load TCGA compendium
 #
-.loadTCGA <- function(nr.datasets=NULL, cache=TRUE, mode=c("ehub", "geo"), 
-    data.dir=NULL, min.ctrls=9, min.cpm=2, with.clin.vars=FALSE, paired=TRUE)
+.loadTCGA <- function(  nr.datasets = NULL, 
+                        cache = TRUE, 
+                        mode = c("ehub", "geo", "cTD"),
+                        data.dir = NULL,
+                        min.ctrls = 9,
+                        min.cpm = 2,
+                        with.clin.vars = FALSE,
+                        paired = TRUE,
+                        map2entrez = TRUE)
 {
     message("Loading TCGA data compendium ...")
     # should a cached version be used?
     if(cache)
     {  
         # ignore all other arguments 
-        el <- .getResourceFromCache(rname="tcga", update.value=NA)
+        el <- .getResourceFromCache(rname="tcga", update.value = NA)
         if(!is.null(el)) return(el[seq_len(min(nr.datasets, length(el)))])
     }      
  
     # otherwise download it
     mode <- match.arg(mode)
     
-    if(mode == "ehub") dat <- .retrieveGSE62944FromEHub(with.clin.vars)
-    else dat <- .downloadGSE62944(data.dir, with.clin.vars)
-    el <- .splitByCancerType(dat$tum, dat$norm, 
-        nr.datasets, min.ctrls=min.ctrls, min.cpm=min.cpm, paired=paired)
-    
+    if(mode == "cTD") 
+        el <- .retrieveFromCTD(nr.datasets, min.ctrls, min.cpm, 
+                                with.clin.vars, paired, map2entrez)
+    else
+    {
+        if(mode == "ehub") dat <- .retrieveGSE62944FromEHub(with.clin.vars)
+        else dat <- .downloadGSE62944(data.dir, with.clin.vars)
+        el <- .splitByCancerType(dat$tum, dat$norm, nr.datasets, 
+                                    min.ctrls, min.cpm, paired, map2entrez)
+    } 
     if(interactive()) cacheResource(el, "tcga")
     return(el)
 }
@@ -38,7 +50,7 @@
 #
 # preferred way of getting GSE62944 (fast)
 #
-.retrieveGSE62944FromEHub <- function(with.clin.vars=FALSE)
+.retrieveGSE62944FromEHub <- function(with.clin.vars = FALSE)
 {
     suppressMessages({
         hub <- ExperimentHub::ExperimentHub()
@@ -66,7 +78,7 @@
 #
 # alternative way of getting GSE62944 in case ExperimentHub fails
 #
-.downloadGSE62944 <- function(data.dir, with.clin.vars=FALSE)
+.downloadGSE62944 <- function(data.dir, with.clin.vars = FALSE)
 {
     # output data directory
     if(is.null(data.dir))
@@ -136,18 +148,133 @@
     list(tum = tum, norm = norm)
 }
 
-.splitByCancerType <- function(tum, norm, 
-    nr.datasets, min.ctrls=9, min.cpm=2, paired=FALSE)
+.retrieveFromCTD <- function(nr.datasets = NULL, min.ctrls = 9, min.cpm = 2, 
+    with.clin.vars = FALSE, paired = TRUE, map2entrez = TRUE)
 {
-    # map IDs hgnc -> entrez
-    suppressMessages({
-        suppressPackageStartupMessages(
-            tum <- EnrichmentBrowser::idMap(tum, 
-                    org="hsa", from="SYMBOL", to="ENTREZID")
-        )
-        norm <- EnrichmentBrowser::idMap(norm, 
-                    org="hsa", from="SYMBOL", to="ENTREZID")
-    })
+    message("Obtaining datasets through curatedTCGAData ...")
+    curatedTCGAData <- experiments <- NULL
+    EnrichmentBrowser::isAvailable("curatedTCGAData", type = "data")
+  
+    rnacomp <- .getResourceFromCache(rname = "cTD_rnacomp")
+    if(is.null(rnacomp))
+    {
+        rnacomp <- curatedTCGAData("*", "RNASeq2*", FALSE)
+        cacheResource(rnacomp, "cTD_rnacomp")
+    }
+
+    # extract the list of SEs
+    exp.list <- experiments(rnacomp)
+    .first <- function(n) unlist(strsplit(n, "_"))[1]    
+    names(exp.list) <- vapply(names(exp.list), .first, character(1))
+
+    # extract samples types (01 = tumor, 11 = adj. normal)
+    stypes <- lapply(exp.list, .getSampleType) 
+    stab <- lapply(stypes, table)
+
+    # has sufficient controls?
+    .hasControls <- function(x) 
+        all(c("01", "11") %in% names(x)) && x["11"] >= min.ctrls
+    ind <- vapply(stab, .hasControls, logical(1))
+    exp.list <- exp.list[ind]
+
+    # specified number of datsets?
+    if(!is.null(nr.datasets)) 
+        exp.list <- exp.list[seq_len(min(nr.datasets, length(exp.list)))]
+
+    exp.list <- lapply(exp.list, .groupSE)
+    for(i in seq_along(exp.list)) 
+        metadata(exp.list[[i]])$dataId <- names(exp.list)[i]   
+    
+    # paired?
+    if(paired)
+    { 
+        exp.list <- lapply(exp.list, .pairSE)
+        nr.samples <- vapply(exp.list, ncol, integer(1)) 
+        exp.list <- exp.list[nr.samples >= min.ctrls * 2]
+    }
+
+    # exclude genes not sufficiently expressed
+    exp.list <- lapply(exp.list, .filterByCPM, min.cpm = min.cpm, is.raw = FALSE)
+
+    # map gene symbols to Entrez Gene IDs?
+    if(map2entrez) 
+        suppressMessages(exp.list <- lapply(exp.list, EnrichmentBrowser::idMap, 
+                                org = "hsa", from = "SYMBOL", to = "ENTREZID"))
+ 
+    # annotate clinical variables?
+    if(with.clin.vars)
+        exp.list <- lapply(exp.list, .extractColData, cdat = colData(rnacomp))
+
+    return(exp.list) 
+}
+
+.filterByCPM <- function(se, min.cpm, is.raw = TRUE)
+{
+    mat <- assay(se)
+    if(is.raw) mat <- edgeR::cpm(mat)
+    rs <- rowSums(mat > min.cpm)
+    keep <- rs >= ncol(se) / 2
+    se <- se[keep,]
+    if(!is.raw)
+    { 
+        assay(se) <- log2(assay(se) + 2)
+        metadata(se)$dataType <- "ma"
+    }
+    return(se)
+}
+
+.getSampleType <- function(se) substring(colnames(se), 14, 15)
+
+.groupSE <- function(se)
+{
+    GRP.COL <- EnrichmentBrowser::configEBrowser("GRP.COL")
+    stypes <- .getSampleType(se)
+    ind <- stypes %in% c("01", "11")
+    se <- se[,ind]
+    stypes <- stypes[ind]
+    se[[GRP.COL]] <- ifelse(stypes == "01", 1, 0) 
+    metadata(se)$dataType <- "rseq"
+    return(se)
+}
+
+.pairSE <- function(se)
+{
+    GRP.COL <- EnrichmentBrowser::configEBrowser("GRP.COL")
+    BLK.COL <- EnrichmentBrowser::configEBrowser("BLK.COL")
+
+    patient.ids <- substring(colnames(se), 1, 12)
+    ctrls <- patient.ids[se[[GRP.COL]] == 0] 
+    cases <- patient.ids[se[[GRP.COL]] == 1] 
+    isect <- intersect(ctrls, cases)    
+    ind <- patient.ids %in% isect
+    se <- se[,ind]
+    se[[BLK.COL]] <- patient.ids[ind] 
+    return(se)
+}
+
+.extractColData <- function(se, cdat)
+{
+    snames <- substring(colnames(se), 1, 12)    
+    colData(se) <- cbind(colData(se), cdat[snames,])
+    return(se)    
+}
+
+.splitByCancerType <- function(tum, norm, nr.datasets, 
+    min.ctrls = 9, min.cpm = 2, paired = FALSE, map2entrez = TRUE)
+{
+    GRP.COL <- EnrichmentBrowser::configEBrowser("GRP.COL")
+    BLK.COL <- EnrichmentBrowser::configEBrowser("BLK.COL")
+
+    if(map2entrez)
+    {
+        # map IDs hgnc -> entrez
+        suppressMessages({
+            tum <- EnrichmentBrowser::idMap(tum, org = "hsa", 
+                                            from = "SYMBOL", to = "ENTREZID")
+            norm <- EnrichmentBrowser::idMap(norm, org = "hsa", 
+                                                from = "SYMBOL", to = "ENTREZID")
+        })
+    }
 
     # creating SEs
     tum.cts <- table(colData(tum)[,"type"])
@@ -201,16 +328,14 @@
             se <- cbind(cases, ctrls) 
             ind <- unique(names(metadata(se)))
             metadata(se) <- metadata(se)[ind]
-            se$GROUP <- c( rep(1, ncol(cases)), rep(0, ncol(ctrls)) )
-            if(paired) se$BLOCK <- rep(isect, 2)
+            se[[GRP.COL]] <- c( rep(1, ncol(cases)), rep(0, ncol(ctrls)) )
+            if(paired) se[[BLK.COL]] <- rep(isect, 2)
             metadata(se)$dataId <- ct 
             metadata(se)$dataType <- "rseq"
 
             # preprocessing
             # rm genes with all 0 and less than min.reads
-            rs <- rowSums(edgeR::cpm(assay(se)) > min.cpm)
-            keep <-  rs >= ncol(se) / 2
-            se[keep,]
+            .filterByCPM(se, min.cpm)
         })
     names(exp.list) <- rel.cts
     
